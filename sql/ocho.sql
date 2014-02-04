97fb1cfed353329a4c27a8f7c9ef969746bab074
umplishk <umplishk@cc.umanitoba.ca>
2014-02-04 01:37:13 -0600

    Beginning sql of database
    Just created a little sql for some light server testing. Uploaded it so
    we can create one common sql file to build upon

---
+++ sql/ocho.sql
@@ -0,0 +1,48 @@
+drop table students;
+drop table professors;
+
+create table STUDENTS(
+	student_id 	BIGINT UNIQUE,
+	username 	VARCHAR(25) NOT NULL,
+	first_name 	VARCHAR(25) NOT NULL,
+	last_name 	VARCHAR(25) NOT NULL,
+	email 		VARCHAR(50) NOT NULL,
+	password 	VARCHAR(50) NOT NULL,
+	settings_id 	BIGINT
+);
+
+alter table STUDENTS add primary key (student_id);
+
+create table PROFESSORS(
+	prof_id 	BIGINT UNIQUE,
+	prof_name 	VARCHAR(50),
+	email 		VARCHAR(50),
+	password	VARCHAR(50),
+	office_hours	VARCHAR(500),
+	settings_id	BIGINT,
+	is_TA		BOOLEAN
+);
+
+alter table PROFESSORS add primary key (prof_id);
+
+insert into STUDENTS values(
+	1,
+	'umplishk',
+	'Alexander',
+	'Plishka',
+	'umplishk@myumanitoba.ca',
+	'password',
+	1
+);
+
+insert into PROFESSORS values(
+	1,
+	'Michael Zapp',
+	'zapp@cs.umanitoba.ca',
+	'password',
+	'None',
+	2,
+	FALSE
+	
+);
+	
