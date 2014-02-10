dd1810d25f05e8e5e6a3aafd582331e111a042d2
Junjie Huang <umcsdon@outlook.com>
2014-02-10 16:02:06 -0600

    clean the master by nico's code

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ var app = express();
 
 // all environments
 app.configure( function() {
-<<<<<<< HEAD
     app.set('port', process.env.PORT || 8080);
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'jade');
@@ -37,21 +36,6 @@ app.configure( function() {
     // If we don't add /public to the path then we can access our stored node_modules
     app.use(express.static(path.join(__dirname, '')));
 });
-=======
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'jade');
-	app.use(express.favicon());
-	app.use(express.logger('dev'));
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	// If we don't add /public to the path then we can access our stored node_modules
-	app.use(express.static(path.join(__dirname, '')));
-});
-
->>>>>>> master
 
 // development only
 if ('development' == app.get('env')) {
@@ -71,8 +55,6 @@ server.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
 
-<<<<<<< HEAD
-
 io.set('authorization', function (handshakeData, accept) {
 
     if (handshakeData.headers.cookie) {
@@ -90,69 +72,7 @@ io.set('authorization', function (handshakeData, accept) {
     }
 
     accept(null, true);
-=======
-io.sockets.on('connection', function(socket) {
-	var dbUrl = "postgres://ocho:ocho@localhost/OchoDb";
-
-	/**
-	*	Database retrievals
-	*/
-	socket.on('getStudent', function(func) {
-		pg.connect(dbUrl, function(err, client, done) {
-			if (err) {
-				return console.error('error fetching client from pool', err);
-			}
-
-			client.query("SELECT * FROM STUDENTS", function(err, result) {
-				// release the client back to the pool				
-				done();
-
-				if (err) {
-					return console.error('error running query', err);
-				}
-
-				console.log("Row count: %d", result.rows.length);
-				
-				socket.emit('foundStudent', result.rows[0]);
-			});
-		});		
-	});
-	
-	socket.on('getProf', function(func) {
-		pg.connect(dbUrl, function(err, client, done) {
-			if (err) {
-				return console.error('error fetching client from pool', err);
-			}
-
-			client.query("SELECT * FROM PROFESSORS", function(err, result) {
-				// release the client back to the pool
-				done();
-
-				if (err) {
-					return console.error('error running query', err);
-				}
-
-				console.log("Row count: %d", result.rows.length);
-				
-				socket.emit('foundProf', result.rows[0]);
-			});
-		});		
-	});
-	
-	socket.on('testios', function(data) {
-		socket.emit('test', { test: 'test' });
-	});
-
-	socket.on('error', function() {
-		console.log('SOCKET ERROR');
-		socket.destroy();
-	});
-
-	socket.on('close', function() {
-		console.log('SOCKET CLOSED');
-	});
->>>>>>> master
-});
+}
 
 io.sockets.on('connection', function(socket) {
     db.url = "postgres://ocho:ocho@localhost/OchoDb";
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 //var hostUrl = 'http://ec2-54-201-56-122.us-west-2.compute.amazonaws.com:8080/';
 var hostUrl = "http://localhost:8080";
-=======
-var hostUrl = 'http://ec2-54-201-56-122.us-west-2.compute.amazonaws.com:3000/';
->>>>>>> master
+
 var socket = io.connect(hostUrl);
 
 $(function() {
