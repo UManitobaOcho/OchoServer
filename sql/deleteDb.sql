07e795bbf46c93d9420db21587ca755ae8fd7cd0
umplishk <umplishk@cc.umanitoba.ca>
2014-02-11 05:31:43 +0000

    Updating ocho.sql so it works. Changed a couple of table names, reformatted the scripts, and added a couple of variables to the tables. Also added test data insertions and a script 'deleteDb' that contains sql to drop all tables.

---
+++ sql/deleteDb.sql
@@ -0,0 +1,15 @@
+drop table completed_tests;
+drop table tests;
+drop table submitted_assignments;
+drop table assignments;
+drop table course_notes;
+drop table course_news;
+drop table course_event;
+drop table forum_posts;
+drop table forum_topic;
+drop table forum;
+drop table enrolled;
+drop table courses;
+drop table students;
+drop table professors;
+drop table saved_settings;
--- sql/ocho.sql
+++ sql/ocho.sql
@@ -1,112 +1,157 @@
-|-----------------------------------------------------|
-| I didn't include all variable with type FILE, and I |
-| also changed Tests --- time_limit to NUMERIC(3)     |
-|-----------------------------------------------------|
-
+/**
+*	Create tables
+*/
 create table SAVED_SETTINGS(
 	settings_id BIGINT UNIQUE primary key,
-	is_prof BOOLEAN
+	is_prof 	BOOLEAN
 );
 
 create table STUDENTS(
-	student_id BIGINT UNIQUE primary key,
-	username VARCHAR(25) NOT NULL,
-	last_name VARCHAR(25) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL
+	student_id 	BIGINT UNIQUE primary key,
+	username 	VARCHAR(25) NOT NULL,
+	first_name	VARCHAR(25) NOT NULL,
+	last_name 	VARCHAR(25) NOT NULL,
+	email 		VARCHAR(50) NOT NULL,
+	password 	VARCHAR(50) NOT NULL,
+	settings_id	BIGINT references SAVED_SETTINGS(settings_id)
 );
 
 create table PROFESSORS(
-	prof_id BIGINT UNIQUE primary key,
-	name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL,
-	office_hours VARCHAR(500) NOT NULL,
-	settings_id BIGINT references SAVED_SETTINGS(settings_id),
-	is_TA BOOLEAN
+	prof_id BIGINT 	UNIQUE primary key,
+	username		VARCHAR(25) NOT NULL,
+	name 			VARCHAR(50) NOT NULL,
+	email 			VARCHAR(50) NOT NULL,
+	password 		VARCHAR(50) NOT NULL,
+	office_hours	VARCHAR(500) NOT NULL,
+	settings_id 	BIGINT references SAVED_SETTINGS(settings_id),
+	is_TA 			BOOLEAN
 );
 
-create table COURSE(
-	course_id BIGINT UNIQUE primary key,
-	course_number VARCHAR(10) NOT NULL,
-	course_section VARCHAR(3) NOT NULL,
-	course_name VARCHAR(50) NOT NULL,
-	prof_id BIGINT references PROFESSORS(prof_id),
-	class_times VARCHAR(200)
+create table COURSES(
+	course_id 		BIGINT UNIQUE primary key,
+	course_number 	VARCHAR(10) NOT NULL,
+	course_section 	VARCHAR(3) NOT NULL,
+	course_name 	VARCHAR(50) NOT NULL,
+	prof_id 		BIGINT references PROFESSORS(prof_id),
+	class_times 	VARCHAR(200)
 );
 
 create table ENROLLED(
 	enrolled_id BIGINT UNIQUE primary key,
-	student_id BIGINT references STUDENTS(student_id),
-	course_id BIGINT references COURSE(course_id),
-	grade NUMERIC(3)
+	student_id 	BIGINT references STUDENTS(student_id),
+	course_id 	BIGINT references COURSES(course_id),
+	grade 		NUMERIC(3)
 );
 
 create table FORUM(
-	forum_id BIGINT UNIQUE primary key,
-	course_id BIGINT references COURSE(course_id)
+	forum_id 	BIGINT UNIQUE primary key,
+	course_id 	BIGINT references COURSES(course_id)
 );
 
 create table FORUM_TOPIC(
-	topic_id BIGINT UNIQUE primary key,
-	forum_id BIGINT references FORUM(forum_id),
-	topic_name VARCHAR(200),
-	post_date DATE NOT NULL
+	topic_id 	BIGINT UNIQUE primary key,
+	forum_id 	BIGINT references FORUM(forum_id),
+	topic_name 	VARCHAR(200),
+	post_date 	DATE NOT NULL
 );
 
 create table FORUM_POSTS(
-	topic_id BIGINT references FORUM_TOPIC(topic_id),
-	student_id BIGINT references STUDENTS(student_id),
-	prof_id BIGINT references PROFESSORS(prof_id),
-	post_VARCHAR VARCHAR(2000)
+	topic_id 		BIGINT references FORUM_TOPIC(topic_id),
+	student_id 		BIGINT references STUDENTS(student_id),
+	prof_id 		BIGINT references PROFESSORS(prof_id),
+	post_content	VARCHAR(2000)
 );
 
 create table COURSE_EVENT(
-	course_id BIGINT references COURSE(course_id),
-	event_name VARCHAR(100) NOT NULL,
-	event_desc VARCHAR(500) NOT NULL,
-	event_time DATE NOT NULL,
-	event_duration DATE NOT NULL
+	course_id 		BIGINT references COURSES(course_id),
+	event_name 		VARCHAR(100) NOT NULL,
+	event_desc 		VARCHAR(500) NOT NULL,
+	event_time 		DATE NOT NULL,
+	event_duration 	DATE NOT NULL
 );
 
 create table COURSE_NEWS(
-	course_id BIGINT references COURSE(course_id),
-	news_title VARCHAR(200) NOT NULL,
-	news_VARCHAR VARCHAR(1000) NOT NULL
+	course_id 		BIGINT references COURSES(course_id),
+	news_title 		VARCHAR(200) NOT NULL,
+	news_content	VARCHAR(1000) NOT NULL
 );
 
 create table COURSE_NOTES(
-	notes_id BIGINT UNIQUE primary key,
-	course_id BIGINT references COURSE(course_id),
-	upload_date DATE NOT NULL,
-	lecture_number NUMERIC(2) NOT NULL,
-	viewable_date DATE NOT NULL
+	notes_id 		BIGINT UNIQUE primary key,
+	course_id 		BIGINT references COURSES(course_id),
+	upload_date 	DATE NOT NULL,
+	lecture_number	NUMERIC(2) NOT NULL,
+	viewable_date 	DATE NOT NULL
 );
 
-create table ASSIGNMENT(
-	assignment_id BIGINT UNIQUE primary key,
-	course_id BIGINT references COURSE(course_id),
-	due_date DATE NOT NULL,
-	viewable_date DATE NOT NULL
+create table ASSIGNMENTS(
+	assignment_id 	BIGINT UNIQUE primary key,
+	course_id 		BIGINT references COURSES(course_id),
+	due_date 		DATE NOT NULL,
+	viewable_date 	DATE NOT NULL
 );
 
-create table SUBMITTED_ASSIGNMENT(
-	enrolled_id BIGINT references ENROLLED(enrolled_id),
-	assignment_id BIGINT references ASSIGNMENT(assignment_id),
+create table SUBMITTED_ASSIGNMENTS(
+	enrolled_id 	BIGINT references ENROLLED(enrolled_id),
+	assignment_id 	BIGINT references ASSIGNMENTS(assignment_id),
 	submission_time DATE NOT NULL,
-	grade NUMERIC(3) NOT NULL
+	grade 			NUMERIC(3) NOT NULL
 );
 
 create table TESTS(
-	test_id BIGINT UNIQUE primary key,
-	course_id BIGINT references COURSE(course_id),
-	viewable_date DATE NOT NULL,
-	test_time DATE NOT NULL,
-	time_limit NUMERIC(3) NOT NULL
+	test_id 		BIGINT UNIQUE primary key,
+	course_id 		BIGINT references COURSES(course_id),
+	viewable_date 	DATE NOT NULL,
+	test_time 		DATE NOT NULL,
+	time_limit 		NUMERIC(3) NOT NULL
 );
 
 create table COMPLETED_TESTS(
 	enrolled_id BIGINT references ENROLLED(enrolled_id),
-	test_id BIGINT references TESTS(test_id),
-	grade NUMERIC(3) NOT NULL
+	test_id 	BIGINT references TESTS(test_id),
+	grade 		NUMERIC(3) NOT NULL
+);
+
+
+/**
+*	Insert initial data
+*/
+insert into SAVED_SETTINGS values(
+	1,
+	FALSE
+);
+
+insert into SAVED_SETTINGS values(
+	2,
+	TRUE
+);
+
+insert into STUDENTS values(
+	1,
+	'umplishk',
+	'Alexander',
+	'Plishka',
+	'umplishk@myumanitoba.ca',
+	'password',
+	1
+);
+
+insert into PROFESSORS values(
+	1,
+	'cszapp',
+	'Michael Zapp',
+	'zapp@cs.umanitoba.ca',
+	'password',
+	'None',
+	2,
+	FALSE
+);
+
+insert into COURSES values(
+	1,
+	'COMP 4350',
+	'A01',
+	'Software Engineering 2',
+	1,
+	'TR 11:30 am - 12:45 pm'
 );
