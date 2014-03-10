501d76951aca31deb38c3a8c2a886fad86aeecb6
ssj3sunny <jasdeepbhumber@gmail.com>
2014-03-10 17:42:08 -0500

    Fix grabbing student already in course

--- public/javascripts/professor.js
+++ public/javascripts/professor.js
@@ -77,7 +77,7 @@ function getProfCourses(){
 function getStudNotInCourse(){
 	console.log("Getting students not in this course");
 	var e = document.getElementById("class-picker");
-	var strClass = e.options[e.selectedIndex].text;
+	var strClass = e.options[e.selectedIndex].val;
 	socket.emit('getStudNotInCourse', {course: strClass});
 	socket.on('foundStudNotInCourse', function(student) {
 		
@@ -99,7 +99,6 @@ function addStudentToCourse() {
 	var students = document.getElementById("student-picker");
 	var e = document.getElementById("class-picker");
 	var strClass = e.options[e.selectedIndex].val;
-	alert(strClass);
 
 	//place if statement if -1 then don't go to server, do something else
 	for(var i = 0;i < students.options.length; i++)
